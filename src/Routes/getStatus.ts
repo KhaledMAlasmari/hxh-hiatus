5de9bae862ce95b51cc9a2fb51b73f4b617a7c20
KhaledMAlasmari <sirkhaled7@gmail.com>
2021-11-02 09:07:46 +0300

    fixed query bug-now it gets latest issue

--- src/Routes/getStatus.ts
+++ src/Routes/getStatus.ts
@@ -4,7 +4,7 @@ import Issue from '../database/Models/Issue'
 const getStatus = Router()
 getStatus.get('/status', async (request, response)  => {
 	try{
-		const {issue, onHiatus, date} = await Issue.findOne().sort({ field: 'asc'}).limit(1).select({date: 1, issue: 1, onHiatus: 1}).exec()
+		const {issue, onHiatus, date} = await Issue.findOne().sort({ field: 'asc', _id: -1 }).limit(1).select({date: 1, issue: 1, onHiatus: 1}).exec()
 		response.status(200).send({issue, onHiatus, date})
 	}
 	catch(error){
